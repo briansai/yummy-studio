b1753b61a39848306cd2968fa11875ad364201f7
Brian Sai <brianwsai@gmail.com>
2023-08-17 18:40:24 -0700

    Made logo responsive to center when screen shrinks

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,24 @@
+'use client';
+import { screenWidth } from '@/constants/screenDim';
 import Image from 'next/image';
+import { useScreenDimensions } from 'use-screen-dimensions';
 
 const NavBar = () => {
+  const { width } = useScreenDimensions();
+
   return (
-    <div className="leading-[10rem] padding-x">
-      <Image src="/logo.png" alt="logo" width={200} height={200} />
+    <div
+      className={`leading-[10rem] padding-x ${
+        width < screenWidth.medium && 'flex-center'
+      }`}
+    >
+      <Image
+        src="/logo.png"
+        alt="logo"
+        width={200}
+        height={200}
+        className="object-contain"
+      />
     </div>
   );
 };
